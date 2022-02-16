15474877fc5250705ffb67840340bb788e8ce13e
syedmahbubuzzaman <syed.mahbub.uz.zaman@g.bracu.ac.bd>
2022-02-17 00:41:57 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './pages/homepage/homepage.styles.scss';
+
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
+ 
